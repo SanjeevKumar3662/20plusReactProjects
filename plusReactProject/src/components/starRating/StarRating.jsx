a8e302acab3e85ddff63da999569fd83da42cb78
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-25 23:35:16 +0530

    now hover and select are separate states

--- plusReactProject/src/components/starRating/StarRating.jsx
+++ plusReactProject/src/components/starRating/StarRating.jsx
@@ -3,10 +3,18 @@ import "./starRating.css";
 
 export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(-1);
-  // const [hover, setHover] = useState(0);
+  const [hover, setHover] = useState(-1);
 
   function handleClickStar(id) {
     setRating(id);
+  }
+
+  function handleMouseEnter(ele) {
+    setHover(ele);
+  }
+
+  function handleMouseLeave() {
+    setHover(0);
   }
 
   const stars = [...Array(noOfStars)].map((_, i) => i + 1);
@@ -14,32 +22,27 @@ export default function StarRating({ noOfStars = 5 }) {
   return (
     <div className="container">
       {stars.map((ele, i) => {
-        if (ele <= rating) {
-          console.log(ele);
-          return (
-            <i
-              key={i}
-              onClick={() => handleClickStar(ele)}
-              onMouseEnter={() => handleClickStar(ele)}
-              onMouseLeave={() => handleClickStar(ele)}
-              style={{ color: "red" }}
-            >
-              ❂
-            </i>
-          );
-        } else {
-          return (
-            <i
-              key={i}
-              onClick={() => handleClickStar(ele)}
-              onMouseEnter={() => handleClickStar(ele)}
-              onMouseLeave={() => handleClickStar(ele)}
-              style={{ color: "black" }}
-            >
-              ❂
-            </i>
-          );
-        }
+        return hover > 0 ? (
+          <i
+            key={i}
+            onClick={() => handleClickStar(ele)}
+            onMouseEnter={() => handleMouseEnter(ele)}
+            onMouseLeave={() => handleMouseLeave()}
+            style={{ color: ele <= hover ? "gold" : "black" }}
+          >
+            ☆
+          </i>
+        ) : (
+          <i
+            key={i}
+            onClick={() => handleClickStar(ele)}
+            onMouseEnter={() => handleMouseEnter(ele)}
+            onMouseLeave={() => handleMouseLeave()}
+            style={{ color: ele <= rating ? "gold" : "black" }}
+          >
+            ☆
+          </i>
+        );
       })}
     </div>
   );
