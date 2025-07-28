b2dedaad1365791f36669149413503955c694fdb
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-28 22:48:44 +0530

    now limit is 10

--- plusReactProject/src/components/loadMoreData/LoadMoreData.jsx
+++ plusReactProject/src/components/loadMoreData/LoadMoreData.jsx
@@ -7,31 +7,34 @@ export default function LoadMoreData() {
   const [count, setCount] = useState(0);
   const [disableLoadBtn, setDisableLoadBtn] = useState(false);
 
-  async function fetchProducts() {
-    try {
-      setLoading(true);
-      const response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${count * 20}`
-      );
-      const result = await response.json();
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://dummyjson.com/products?limit=10&skip=${count === 0? 0 :count * 10}`
+        );
+        const result = await response.json();
 
-      if (result && result.products && result.products.length) {
-        setProducts((prevData) => [...prevData, ...result.products]);
+        if (result && result.products && result.products.length) {
+          setProducts((prevData) => [...prevData, ...result.products]);
+          setLoading(false);
+        }
+      } catch (e) {
+        console.log(e);
         setLoading(false);
       }
-    } catch (e) {
-      console.log(e);
-      setLoading(false);
     }
-  }
-
-  useEffect(() => {
     fetchProducts();
   }, [count]);
 
-  useEffect(()=>{if(count>=5){setDisableLoadBtn(true)}} , [count]);
+  useEffect(() => {
+    if (count >= 5) {
+      setDisableLoadBtn(true);
+    }
+  }, [count]);
 
-  if (loading === true) {
+  if (loading !== false) {
     return <div>Loading data ! Please Wait</div>;
   }
 
@@ -42,7 +45,7 @@ export default function LoadMoreData() {
       <div className="loadMoreData-Container">
         {products && products.length
           ? products.map((ele) => {
-              console.log(ele);
+              // console.log(ele);
               return (
                 <div className="product" key={ele.id}>
                   <img src={ele.thumbnail} alt={ele.title} height={200} />
@@ -53,8 +56,12 @@ export default function LoadMoreData() {
           : null}
       </div>
       <div className="button-container">
-        <button onClick={() => setCount(() => count + 1)} disabled={disableLoadBtn}>
-          {disableLoadBtn?"Limit Exceeded !!":"Load More"}</button>
+        <button
+          onClick={() => setCount(() => count + 1)}
+          disabled={disableLoadBtn}
+        >
+          {disableLoadBtn ? "Limit Exceeded !!" : "Load More"}
+        </button>
       </div>
     </div>
   );
