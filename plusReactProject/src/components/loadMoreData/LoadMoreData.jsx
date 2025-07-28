094c00ad88699fe4cf33fa8cd2e2f43af09b0db8
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-28 21:59:41 +0530

    now by default 20 products will be added load button add more 20

--- plusReactProject/src/components/loadMoreData/LoadMoreData.jsx
+++ plusReactProject/src/components/loadMoreData/LoadMoreData.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./loadMoreData.css";
 
 export default function LoadMoreData() {
   const [loading, setLoading] = useState(false);
@@ -7,18 +8,50 @@ export default function LoadMoreData() {
 
   async function fetchProducts() {
     try {
-      const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count * 20}`);
-      const data = response.json;
-      console.log(data);
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${count * 20}`
+      );
+      const result = await response.json();
 
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
     } catch (e) {
       console.log(e);
+      setLoading(false);
     }
   }
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [count]);
 
-  return <div className="container"></div>;
+  if (loading === true) {
+    return <div>Loading data ! Please Wait</div>;
+  }
+
+  // console.log(products);
+
+  return (
+    <div className="container">
+      <div className="loadMoreData-Container">
+        {products && products.length
+          ? products.map((ele) => {
+              console.log(ele);
+              return (
+                <div className="product" key={ele.id}>
+                  <img src={ele.thumbnail} alt={ele.title} height={200} />
+                  <p>{ele.title}</p>
+                </div>
+              );
+            })
+          : null}
+      </div>
+      <div className="button-container">
+        <button onClick={() => setCount(() => count + 1)}>Load More</button>
+      </div>
+    </div>
+  );
 }
