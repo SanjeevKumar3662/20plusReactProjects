9900776c351d7db0be24b65c5bd2f13625849071
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-16 16:25:39 +0530

    every prject and restarting from start

--- plusReactProject/src/App.jsx
+++ plusReactProject/src/App.jsx
@@ -1,22 +1,20 @@
 // import { useState } from 'react'
-import Accordion from './components/accordion/Accordion'
-import RandomColor from './components/randomColorGen/RandomColor'
-import StarRating from './components/starRating/StarRating'
-import Slider from './components/slider/Slider'
-import LoadMoreData from './components/loadMoreData/LoadMoreData'
-
+import Accordion from "./components/accordion/Accordion";
+// import RandomColor from "./components/randomColorGen/RandomColor";
+// import StarRating from "./components/starRating/StarRating";
+// import Slider from "./components/slider/Slider";
+// import LoadMoreData from "./components/loadMoreData/LoadMoreData";
 
 function App() {
-
   return (
     <>
       <Accordion></Accordion>
-      <RandomColor></RandomColor>
-      <StarRating noOfStars={7}></StarRating>
-      <Slider limit={5}></Slider>
-      <LoadMoreData></LoadMoreData>
+      {/* <RandomColor></RandomColor> */}
+      {/* <StarRating noOfStars={7}></StarRating> */}
+      {/* <Slider limit={5}></Slider> */}
+      {/* <LoadMoreData></LoadMoreData> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- plusReactProject/src/components/accordion/Accordion.jsx
+++ plusReactProject/src/components/accordion/Accordion.jsx
@@ -3,67 +3,16 @@ import data from "./data";
 import { useState } from "react";
 
 export default function Accordion() {
-  const [selected, setSelected] = useState(null);
-  const [isSingleMode, setMode] = useState(true);
-  const [multiSelect, setMultiSelect] = useState([]);
-
-  function handleSingleSelect(id) {
-    console.log(id);
-    if (isSingleMode) {
-      id === selected ? setSelected(null) : setSelected(id);
-    } else {
-      multiSelect.includes(id)
-        ? setMultiSelect(()=> multiSelect.filter((ele) => ele != id))
-        : setMultiSelect(() => [...multiSelect, id]);
-      // id === selected?setSelected(null):setSelected(id);
-    }
-  }
+  
 
   return (
-    <div className="container">
+    <div className="t-container flex flex-col justify-center items-center">
       <h1 className="text-3xl">Project 1 : Accordion</h1>
-      <button
-        onClick={() => setMode(() => !isSingleMode)}
-        className=" border-solid border-black border-1 rounded-md p-1"
-      >
-        {isSingleMode?"Single Select Mode":"Multi Select Mode"}
-      </button>
-
-      {data && data.length > 0
-        ? data.map((ele) => {
-            return isSingleMode ? (
-              <div key={ele.id} className="item">
-                <div
-                  onClick={() => handleSingleSelect(ele.id)}
-                  className="title"
-                >
-                  {ele.question}
-                </div>
-                
-                {selected === ele.id ? (
-                  <div className="answer">{ele.answer}</div>
-                ) : (
-                  <span></span>
-                )}
-              </div>
-            ) : (
-              <div key={ele.id} className="item">
-                <div
-                  onClick={() => handleSingleSelect(ele.id)}
-                  className="title"
-                >
-                  {ele.question}
-                </div>
-                
-                {multiSelect.includes(ele.id) ? (
-                  <div className="answer">{ele.answer}</div>
-                ) : (
-                  <span></span>
-                )}
-              </div>
-            );
-          })
-        : "no"}
+      <button className="border-solid border-1 p-1 rounded-md">Toggle Button</button>
+    
+    {data&& data.map(ele=>{
+      <span>{ele.id}</span>
+    })}
     </div>
   );
 }
