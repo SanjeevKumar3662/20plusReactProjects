fd2dd6370bcbfbb4277b126530dcd56635638126
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-16 17:48:06 +0530

    re-made accordion component from scratch

--- plusReactProject/src/components/accordion/Accordion.jsx
+++ plusReactProject/src/components/accordion/Accordion.jsx
@@ -3,16 +3,74 @@ import data from "./data";
 import { useState } from "react";
 
 export default function Accordion() {
-  
+  const [toggleStatus, setToggleStatus] = useState("single"); //single or multi
+  const [isSingleSelected, setSingleSeleted] = useState(null);
+  const [isMultiSelect, setMultiSelect] = useState(new Array(0).fill(0));
+
+  function handleClick(id) {
+    if (toggleStatus === "single") {
+      setSingleSeleted(id === isSingleSelected ? null : id);
+    } else if (toggleStatus === "multi") {
+      const index = isMultiSelect.indexOf(id);
+
+      setMultiSelect(
+        index === -1
+          ? [...isMultiSelect, id]
+          : isMultiSelect.filter((ele) => ele !== id)
+      );
+    }
+  }
 
   return (
-    <div className="t-container flex flex-col justify-center items-center">
+    <div className="t-container flex flex-col justify-center items-center gap-3">
       <h1 className="text-3xl">Project 1 : Accordion</h1>
-      <button className="border-solid border-1 p-1 rounded-md">Toggle Button</button>
-    
-    {data&& data.map(ele=>{
-      <span>{ele.id}</span>
-    })}
+      <button
+        onClick={() =>
+          setToggleStatus(() =>
+            toggleStatus === "single" ? "multi" : "single"
+          )
+        }
+        className="border-solid border-1 p-1 rounded-md"
+      >
+        Toggle Button
+      </button>
+
+      {data &&
+        data.map((ele) => {
+          {
+            return (
+              <div
+                onClick={() => handleClick(ele.id)}
+                key={ele.id}
+                className="item gap-5"
+              >
+                <span>{ele.question}</span>
+
+                {(() => {
+                  switch (toggleStatus) {
+                    case "single":
+                      return (
+                        isSingleSelected === ele.id && (
+                          <p className="bg-blue-200 p-3 rounded-md">
+                            {ele.answer}
+                          </p>
+                        )
+                      );
+
+                    case "multi":
+                      return (
+                        isMultiSelect.indexOf(ele.id) !== -1 && (
+                          <p className="bg-blue-200 p-3 rounded-md">
+                            {ele.answer}
+                          </p>
+                        )
+                      );
+                  }
+                })()}
+              </div>
+            );
+          }
+        })}
     </div>
   );
 }
