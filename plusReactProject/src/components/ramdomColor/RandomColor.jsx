e5be8b2f3589601ee2f91a4ad2fe0beee4c9941b
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-17 23:34:10 +0530

    created the base structure for RandomColor Component, (currently it's static with no function)

--- plusReactProject/src/components/ramdomColor/RandomColor.jsx
+++ plusReactProject/src/components/ramdomColor/RandomColor.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+// import React, { useState } from "react";
 
 const RandomColor = () => {
   return (
-    <div className="t-container">
-      <h1 className='t-h1'>Project 2 : Random Color Generator</h1>
+    <div
+      className="t-container flex flex-col justify-center items-center gap-3"
+      style={{ backgroundColor: `${"blue"}` }}
+    >
+      <h1 className="t-h1">Project 2 : Random Color Generator</h1>
+      <div className="">
+        Select Color Type
+        <button className="btn">Hex Color</button>
+        <button className="btn">RGB Color</button>
+      </div>
+      <button className="btn">Generate</button>
     </div>
-  )
-}
+  );
+};
 
-export default RandomColor
+export default RandomColor;
