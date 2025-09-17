6c282deff2524fa43f3a33de89e7a4deca43d3c8
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-17 23:56:16 +0530

    now hex color generates, added the functionality in handleGenClick function

--- plusReactProject/src/components/ramdomColor/RandomColor.jsx
+++ plusReactProject/src/components/ramdomColor/RandomColor.jsx
@@ -1,18 +1,45 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 const RandomColor = () => {
+  const [curType, setCurType] = useState("Hex");
+  const [color, setColor] = useState("white");
+
+  const handleGenClick = () => {
+    if (curType === "Hex") {
+      const hexCode = "0123456789ABCDEF".split("");
+
+      let code = "";
+      for (let i = 0; i < 6; i++) {
+        code += hexCode[Math.floor(Math.random() * 15)];
+        console.log(code);
+      }
+      setColor(`#${code}`);
+    } else {
+      setColor("green");
+    }
+  };
+
   return (
     <div
       className="t-container flex flex-col justify-center items-center gap-3"
-      style={{ backgroundColor: `${"blue"}` }}
+      style={{ backgroundColor: `${color}` }}
     >
       <h1 className="t-h1">Project 2 : Random Color Generator</h1>
       <div className="">
         Select Color Type
-        <button className="btn">Hex Color</button>
-        <button className="btn">RGB Color</button>
+        <button onClick={() => setCurType("Hex")} className="btn">
+          Hex Color
+        </button>
+        <button onClick={() => setCurType("rgb")} className="btn">
+          RGB Color
+        </button>
       </div>
-      <button className="btn">Generate</button>
+      <div>
+        Currently {curType} is Selected :{" "}
+        <button onClick={handleGenClick} className="btn">
+          Generate
+        </button>
+      </div>
     </div>
   );
 };
