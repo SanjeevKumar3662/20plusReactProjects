98b6fdabd3fdf41ce3ae786667a857a559676ba1
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-18 18:21:52 +0530

    now on click starRating works

--- plusReactProject/src/components/starRating/StarRating.jsx
+++ plusReactProject/src/components/starRating/StarRating.jsx
@@ -1,20 +1,38 @@
-import React, { startTransition } from "react";
+import React, { useState } from "react";
 
 const StarRating = ({ noOfStars }) => {
+  const [rating, setRating] = useState(-1);
+  // const [hoverRating, setHoverRating] = useState(-1);
+
   const stars = new Array(noOfStars).fill(0);
-  console.log(stars);
+  // console.log(stars);
 
   return (
     <div className="t-container">
       <h1 className="t-h1">Project 3 : Star Rating</h1>
       <h1>no of stars : {noOfStars}</h1>
 
-      <div className="border-1">{
-          stars.map((ele,index)=>{
-            return <span key={index} className="text-4xl">☆</span>
-          })
-        }
-        </div>
+      <div className="border-1 rounded-2xl px-3">
+        {stars.map((ele, index) => {
+          return index <= rating ? (
+            <span
+              onClick={() => setRating(index)}
+              key={index}
+              className="text-8xl text-amber-300 font-semi-bold"
+            >
+              ☆
+            </span>
+          ) : (
+            <span
+              onClick={() => setRating(index)}
+              key={index}
+              className="text-8xl  font-semi-bold"
+            >
+              ☆
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 };
