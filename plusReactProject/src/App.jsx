6bf8cf975f80cc961aab793db5f8105f068ca33f
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-18 16:06:35 +0530

    imported/added  StarComponent in App.jsx

--- plusReactProject/src/App.jsx
+++ plusReactProject/src/App.jsx
@@ -1,13 +1,15 @@
 // import { useState } from 'react'
 import Accordion from "./components/accordion/Accordion";
 import RandomColor from "./components/ramdomColor/RandomColor";
+import StarRating from "./components/starRating/StarRating";
+
 
 function App() {
   return (
     <>
       <Accordion></Accordion>
       <RandomColor></RandomColor>
-      {/* <StarRating noOfStars={7}></StarRating> */}
+      <StarRating noOfStars={7}></StarRating>
       {/* <Slider limit={5}></Slider> */}
       {/* <LoadMoreData></LoadMoreData> */}
     </>
