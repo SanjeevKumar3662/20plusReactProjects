856ba5214e6c63f65e400895372d5078ec4d5db1
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-18 18:10:25 +0530

    now according to noOfStars prop, stars are created

--- plusReactProject/src/components/starRating/StarRating.jsx
+++ plusReactProject/src/components/starRating/StarRating.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { startTransition } from "react";
 
-const StarRating = ({noOfStars}) => {
+const StarRating = ({ noOfStars }) => {
+  const stars = new Array(noOfStars).fill(0);
+  console.log(stars);
+
   return (
     <div className="t-container">
-      <h1 className='t-h1' >Project 3 : Star Rating</h1>
+      <h1 className="t-h1">Project 3 : Star Rating</h1>
       <h1>no of stars : {noOfStars}</h1>
-    </div>
-  )
-}
 
-export default StarRating
+      <div className="border-1">{
+          stars.map((ele,index)=>{
+            return <span key={index} className="text-4xl">☆</span>
+          })
+        }
+        </div>
+    </div>
+  );
+};
+
+export default StarRating;
