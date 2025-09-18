b71f07733bf9162469b3cfcaf73bba66b6f29344
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-18 16:05:34 +0530

    added StarRating component in components folder

---
+++ plusReactProject/src/components/starRating/StarRating.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StarRating = ({noOfStars}) => {
+  return (
+    <div className="t-container">
+      <h1 className='t-h1' >Project 3 : Star Rating</h1>
+      <h1>no of stars : {noOfStars}</h1>
+    </div>
+  )
+}
+
+export default StarRating
