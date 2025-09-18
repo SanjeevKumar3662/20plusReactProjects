04b2e7c706db7b92844cf4995af5ca877a03b873
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-19 00:37:23 +0530

    added a blank imageSlider component

---
+++ plusReactProject/src/components/imageSlider/ImageSlider.jsx
@@ -0,0 +1,10 @@
+const ImageSlider = ({limit}) => {
+  return (
+    <div className="t-container">
+      <h1 className="t-h1">Project 4 : ImageSlider</h1>
+      <h2>No. of Images : {limit}</h2>
+    </div>
+  );
+};
+
+export default ImageSlider;
