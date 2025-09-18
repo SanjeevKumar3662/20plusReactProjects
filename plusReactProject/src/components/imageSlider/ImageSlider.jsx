9418685926144d3e9e0ffaa3e34d7bf40a31d85b
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-19 00:58:45 +0530

    currently fetching images and show in a line

--- plusReactProject/src/components/imageSlider/ImageSlider.jsx
+++ plusReactProject/src/components/imageSlider/ImageSlider.jsx
@@ -1,8 +1,30 @@
-const ImageSlider = ({limit}) => {
+import { useEffect, useState } from "react";
+
+const ImageSlider = ({ limit }) => {
+  const [images, setImages] = useState(null);
+  const url = `https://picsum.photos/v2/list?page=1&limit=${limit}`;
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setImages(data);
+    };
+    fetchImages();
+  }, []);
+
   return (
     <div className="t-container">
       <h1 className="t-h1">Project 4 : ImageSlider</h1>
       <h2>No. of Images : {limit}</h2>
+      <div className="flex">
+        {images &&
+          images.length > 0 &&
+          images.map((ele) => {
+            return <img key={ele.id} src={ele.download_url} alt="" />;
+          })}
+      </div>
     </div>
   );
 };
