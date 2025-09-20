0ffcad495bd5dac3222e88aaef82c5ab04f3fad2
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-20 21:12:59 +0530

    added MenuList to TreeView Component

--- plusReactProject/src/components/treeView/TreeView.jsx
+++ plusReactProject/src/components/treeView/TreeView.jsx
@@ -1,7 +1,15 @@
+import menu from "./data";
+import MenuList from "./MenuList";
 const TreeView = () => {
+  // console.log(menu);
   return (
-    <div className="t-container">
-      <h1 className="t-h1">Project 6 : Tree View</h1>
+    <div className="t-container p-0">
+      <h1>Project 6 : Tree View</h1>
+      <div className=" item border flex flex-col items-start ">
+        {menu.map((ele) => {
+          return <MenuList {...ele} level={0} key={ele.label} />;
+        })}
+      </div>
     </div>
   );
 };
