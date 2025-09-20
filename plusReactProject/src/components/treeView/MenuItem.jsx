381b51713963eb98781f90c86d02ab275ad36425
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-20 21:09:59 +0530

    added MenuItem Component to MenuList, MenuItem calls itself for every children element

---
+++ plusReactProject/src/components/treeView/MenuItem.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import MenuList from "./MenuList";
+
+const MenuItem = ({ label, children, level }) => {
+  const [toOpen, setToOpen] = useState(false);
+  const handleClickMenu = (e) => {
+    e.stopPropagation();
+    setToOpen(!toOpen);
+  };
+  return (
+    <div
+      className="  flex flex-col gap-2 my-2 
+      "
+      style={{ marginLeft: `${level * 50}px` }}
+      onClick={(e) => handleClickMenu(e)}
+    >
+      <span className={` bg-blue-400 p-3 rounded-xl`}>
+        Level : {level} --
+        {label} {children && "+"}
+      </span>
+      <div className={`${toOpen ? "block" : "hidden"}`}>
+        {children &&
+          children.map((ele) => (
+            <MenuItem key={ele.label} {...ele} level={level + 1} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
