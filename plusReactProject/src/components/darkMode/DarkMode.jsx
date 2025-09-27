a1195ba82d3961d2db7f3d2ea94eb4442a043ef5
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-27 17:24:00 +0530

    created basic DarkMode component

---
+++ plusReactProject/src/components/darkMode/DarkMode.jsx
@@ -0,0 +1,10 @@
+const DarkMode = () => {
+  return (
+    <div className="t-container">
+      <h1 className="t-h1">Project 8 : DarkMode</h1>
+      <button className="btn ">Change Theme</button>
+    </div>
+  );
+};
+
+export default DarkMode;
