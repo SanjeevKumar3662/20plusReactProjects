e00eed15a5f856d4931773aff67c5222542e6264
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-27 18:55:03 +0530

    now DarkMode component uses local storage for checking status of darkmode

--- plusReactProject/src/components/darkMode/DarkMode.jsx
+++ plusReactProject/src/components/darkMode/DarkMode.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 const DarkMode = () => {
+  const [isDarkMode, setDarkMode] = useState(
+    () => localStorage.getItem("darkMode") === "true"
+  );
+
+  function onClickButton(status) {
+    setDarkMode(() => status);
+    localStorage.setItem("darkMode", status);
+  }
+
   return (
-    <div className="t-container">
+    <div
+      className={`t-container ${
+        isDarkMode ? "bg-black text-white" : "bg-white text-black"
+      }`}
+    >
       <h1 className="t-h1">Project 8 : DarkMode</h1>
-      <button className="btn ">Change Theme</button>
+      <span>Status in Local Storage ? {localStorage.getItem("darkMode")}</span>
+      <button onClick={() => onClickButton(!isDarkMode)} className="btn ">
+        Change Theme
+      </button>
     </div>
   );
 };
